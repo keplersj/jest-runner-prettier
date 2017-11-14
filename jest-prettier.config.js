f02879a24178734089f35a4463fcd5a2aecfc45c
Kepler Sticka-Jones <kepler+git@stickajones.org>
2017-11-14 13:38:40 -0700

    Initial commit.
    Basic working implementation with a Snapshot-inspired diff output. Can't seem to get diff coloring working without setting FORCE_COLOR though. Will investigate

---
+++ jest-prettier.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  runner: "./src/index.js",
+  displayName: "prettier",
+  testMatch: [
+    "**/*.js",
+    "**/*.jsx",
+    "**/*.json",
+    "**/*.ts",
+    "**/*.tsx",
+    "**/*.css",
+    "**/*.less",
+    "**/*.scss",
+    "**/*.graphql",
+    "**/*.md",
+    "**/*.markdown"
+  ]
+};
---
+++ src/run.js
@@ -0,0 +1,30 @@
+const chalk = require("chalk");
+const { pass, fail } = require("create-jest-runner");
+const fs = require("fs");
+const diff = require("jest-diff");
+const prettier = require("prettier");
+
+module.exports = ({ testPath, config, globalConfig }) => {
+  const start = new Date();
+  const contents = fs.readFileSync(testPath, "utf8");
+
+  const isPretty = prettier.check(contents, {filepath: testPath});
+
+  if (isPretty) {
+    return pass({
+      start,
+      end: new Date(),
+      test: { path: testPath }
+    });
+  }
+
+  const formatted = prettier.format(contents, {filepath: testPath});
+
+  return fail({
+    start,end: new Date(),
+    test: {
+      path: testPath,
+      errorMessage: diff(formatted, contents, {expand: false}),
+    }
+  });
+};
