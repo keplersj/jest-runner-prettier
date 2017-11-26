ed2ba86d0ed903d81079d558ab7d96af7b9613a7
Haukur Páll Hallvarðsson <hph@hph.is>
2017-11-26 15:38:16 +0000

    feat: add support for custom Prettier config

--- src/run.js
+++ src/run.js
@@ -9,26 +9,31 @@ module.exports = ({ testPath, config, globalConfig }) => {
   const start = new Date();
   const contents = fs.readFileSync(testPath, "utf8");
 
-  const isPretty = prettier.check(contents, { filepath: testPath });
+  return prettier.resolveConfig(testPath).then(config => {
+    const prettierConfig = Object.assign({}, config, {
+      filepath: testPath
+    });
 
-  if (isPretty) {
-    return pass({
+    const isPretty = prettier.check(contents, prettierConfig);
+    if (isPretty) {
+      return pass({
+        start,
+        end: new Date(),
+        test: { path: testPath }
+      });
+    }
+
+    const formatted = prettier.format(contents, prettierConfig);
+
+    return fail({
       start,
       end: new Date(),
-      test: { path: testPath }
+      test: {
+        path: testPath,
+        errorMessage: diff(highlight(formatted), highlight(contents), {
+          expand: false
+        })
+      }
     });
-  }
-
-  const formatted = prettier.format(contents, { filepath: testPath });
-
-  return fail({
-    start,
-    end: new Date(),
-    test: {
-      path: testPath,
-      errorMessage: diff(highlight(formatted), highlight(contents), {
-        expand: false
-      })
-    }
   });
 };
