f5c31e1b300227771fcd5b201a66c0c87677d172
Kepler Sticka-Jones <kepler@stickajones.org>
2022-02-26 23:42:53 -0700

    refactor: use async await in test runner

--- src/run.ts
+++ src/run.ts
@@ -1,7 +1,7 @@
 import { TestResult } from "@jest/test-result";
 import { emphasize } from "emphasize";
 import { pass, fail } from "create-jest-runner";
-import fs from "fs";
+import * as fs from "fs/promises";
 import { diff } from "jest-diff";
 import prettier from "prettier";
 
@@ -9,39 +9,38 @@ interface Parameters {
   testPath: string;
 }
 
-export default ({ testPath }: Parameters): Promise<TestResult> => {
+export default async ({ testPath }: Parameters): Promise<TestResult> => {
   const start = new Date().getTime();
-  const contents = fs.readFileSync(testPath, "utf8");
+  const contents = await fs.readFile(testPath, "utf8");
+  const config = await prettier.resolveConfig(testPath);
 
-  return prettier.resolveConfig(testPath).then((config) => {
-    const prettierConfig = Object.assign({}, config, {
-      filepath: testPath,
-    });
+  const prettierConfig = Object.assign({}, config, {
+    filepath: testPath,
+  });
 
-    const isPretty = prettier.check(contents, prettierConfig);
-    if (isPretty) {
-      return pass({
-        start,
-        end: new Date().getTime(),
-        test: { path: testPath },
-      });
-    }
-
-    const formatted = prettier.format(contents, prettierConfig);
-
-    return fail({
+  const isPretty = prettier.check(contents, prettierConfig);
+  if (isPretty) {
+    return pass({
       start,
       end: new Date().getTime(),
-      test: {
-        path: testPath,
-        errorMessage: diff(
-          emphasize.highlightAuto(formatted).value,
-          emphasize.highlightAuto(contents).value,
-          {
-            expand: false,
-          }
-        ),
-      },
+      test: { path: testPath },
     });
+  }
+
+  const formatted = prettier.format(contents, prettierConfig);
+
+  return fail({
+    start,
+    end: new Date().getTime(),
+    test: {
+      path: testPath,
+      errorMessage: diff(
+        emphasize.highlightAuto(formatted).value,
+        emphasize.highlightAuto(contents).value,
+        {
+          expand: false,
+        }
+      ),
+    },
   });
 };
